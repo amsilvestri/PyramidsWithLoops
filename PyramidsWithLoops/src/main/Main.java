17485c00d44998eb3c6a8eca39ca9ba596d0613f
silvesam <silvesam@LCBLAB2110-35.uccob.uc.edu>
2019-09-19 09:17:37 -0400

    Commit 9/19/2019

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -8,19 +8,30 @@
       33333
      4444444
     555555555 
-    to get code back to remote repo( edit code first) right click on project->team-> commit
+    to get code back to remote repo ( edit code first) right click on project->team-> commit
 */
 package main;
 
 public class Main {
 
 	public static void main(String[] args) {
-		int lines = 5;
-		for (int i = 0; i < lines; i++) {
+		int lines = 5, j = 0;
+        for(int i = 1; i <= lines; ++i, j = 0) {
+            for(int space = 1; space <= lines - i; ++space) {
+                System.out.print("  ");
+            }
+            while(j != 2 * i - 1) {
+                System.out.print (i );
+                ++j;
+            }
+            System.out.println();
+		
+		/*int lines = 5;
+		for (int i= 0; i < lines; i++) {
 			for (int j = 0; j < i+1; j++) {
 				System.out.print(j);
 			}
-			System.out.println("");
+			System.out.println("");*/
 		}
 	}
 }
