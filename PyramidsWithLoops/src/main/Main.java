72a60754fe6235c747c812a77bc52eabaa4d6f0e
amsil <amsil@DESKTOP-T55HNUV.home>
2019-09-23 18:02:03 -0400

    Finished

--- PyramidsWithLoops/src/main/Main.java
+++ PyramidsWithLoops/src/main/Main.java
@@ -15,23 +15,25 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-		int lines = 5, j = 0;
-        for(int i = 1; i <= lines; ++i, j = 0) {
-            for(int space = 1; space <= lines - i; ++space) {
-                System.out.print("  ");
-            }
-            while(j != 2 * i - 1) {
-                System.out.print (i );
-                ++j;
-            }
-            System.out.println();
 		
-		/*int lines = 5;
-		for (int i= 0; i < lines; i++) {
-			for (int j = 0; j < i+1; j++) {
-				System.out.print(j);
+		int n = 5; int rownum;// setting the number of rows
+		for(int i=0; i<n; i++)//outer loop
+		{
+			for (int j=n-i; j>1; j--)// this for loop is for the rows(inner loop)
+			{
+				System.out.print(" ");//this prints the spaces at the beginning of each row
 			}
-			System.out.println("");*/
+			rownum =i+1; {System.out.print(rownum);}// printing each number per row
+			
+			
+			for (int j=1; j<=i; j++ ) //this for loop is for the columns (inner loop)
+			{ 
+				System.out.print(rownum + "" +  rownum);// this command prints the numbers
+			}
+			System.out.println(); //this creates a new line of numbers after each row
 		}
+         	
+		
 	}
+		    
 }
